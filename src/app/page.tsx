31a2c0f577cb3706bc217c6ae1f83b16231f4fe5
Harshit-087 <harshit283773@gmail.com>
2025-07-03 12:16:17 +0530

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 "use client"
-import React,{useEffect} from 'react'
+
 import {redirect} from "next/navigation"
 
 export default function (){
     redirect("/home")
 }
 
-// export const Page = () => {
-//  const router =useRouter()
-//  useEffect(()=>{
-//   router.push("home")
-//  })
- 
-// }
+
 
  
